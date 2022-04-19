7118465a0ffe537ef8fe5a5a489b01a7b3c8ac10
Dylan Crompton <easyd313@gmail.com>
2022-04-19 18:48:37 -0400

    added notepad icon and app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import start_button from './assets/start_button.png';
+import notepad_icon from './assets/notepad_icon.jpg';
+
 
 function App() {
   return (
     <div className="background">
-      
+      <img src={notepad_icon} alt="notepad" className="notepad_icon" />
+      <div className="notepad">
+        <div className="topBar"></div>
+      </div>
 
       <div className="start_menu">
-        <img src={start_button} alt="start button" className="start_button"/>
+        <img src={start_button} alt="start button" className="start_button" />
       </div>
     </div>
   );
