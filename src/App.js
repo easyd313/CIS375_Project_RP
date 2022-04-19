71631bc5d6893e889ca84d42c37811e3ba0d38f4
Dylan Crompton <easyd313@gmail.com>
2022-04-19 17:41:42 -0400

    added background and start menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import start_button from './assets/start_button.png';
 
 function App() {
   return (
     <div className="background">
       
 
-      <div className="start_menu"></div>
+      <div className="start_menu">
+        <img src={start_button} alt="start button" className="start_button"/>
+      </div>
     </div>
   );
 }
